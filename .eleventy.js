ebca1a699df497aeb8b0fd7f9da89e69214a4ffc
planobioblogpost <planobioblogpost@gmail.com>
2025-09-18 20:05:31 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,19 @@
 module.exports = function(eleventyConfig) {
   // Passa as pastas de CSS, imagens e admin para o site final
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("imagens"); // Criaremos esta pasta mais tarde
+  eleventyConfig.addPassthroughCopy("imagens");
   eleventyConfig.addPassthroughCopy("admin");
+
+  // A INSTRUÇÃO ESSENCIAL QUE CORRIGE O ERRO
+  // Isto ensina o Eleventy a substituir {% year %} pelo ano atual.
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
       input: ".",
       includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
       output: "_site",
     },
     templateFormats: ["md", "njk", "html"],
