1bd45ec5f67dbdbff2b9421e046eff68d2921952
planobioblogpost <planobioblogpost@gmail.com>
2025-09-19 10:45:29 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
 module.exports = function(eleventyConfig) {
+  // Passa as pastas de CSS, imagens e admin para o site final
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("imagens");
   eleventyConfig.addPassthroughCopy("admin");
 
+  // Adiciona um "shortcode" para que possamos usar {% year %} nos templates
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // ===================================================================
+  // A NOVA LIÇÃO: A DEFINIÇÃO DO FILTRO "LIMIT"
+  // ===================================================================
+  // Isto ensina o Eleventy a entender "limit(3)" e pegar apenas os 3 primeiros itens de uma lista.
+  eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
 
   return {
     dir: {
